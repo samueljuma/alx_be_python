900dd880be1f962027f4f29e5e4fdd6791c14a2d
samueljuma <samueljuma1024@gmail.com>
2025-01-11 12:35:34 +0300

    modify arithmetic_operationa.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return f"{num1 + num2}"
-        case "subtract":
-            return f"{num1 - num2}"
-        case "multiply":
-            return f"{num1 * num2}"
-        case "divide":
-            if num2 == 0:
-                return "You cannot divide by zero."
-            else:
-                return f"{num1 / num2}"
-              
-        case _:
-            return "Invalid operation"
+    if operation == "add":
+        return f"{num1 + num2}"
+    elif operation == "subtract":
+        return f"{num1 - num2}"
+    elif operation == "multiply":
+        return f"{num1 * num2}"
+    elif operation == "divide":
+        if num2 == 0:
+            return "You cannot divide by zero."
+        else:
+            return f"{num1 / num2}"
+    else:
+        return "Invalid operation"
