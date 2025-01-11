bac8641e90c02a94c332c609c60d6c6f3b53e9f6
samueljuma <samueljuma1024@gmail.com>
2025-01-11 12:18:09 +0300

    Add solution for Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return f"{num1 + num2}"
+        case "subtract":
+            return f"{num1 - num2}"
+        case "multiply":
+            return f"{num1 * num2}"
+        case "divide":
+            if num2 == 0:
+                return "You cannot divide by zero."
+            else:
+                return f"{num1 / num2}"
